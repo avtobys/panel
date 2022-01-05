5c39c08a05945f21fce5168bbd1d7e37e669a238
avtobys <avtobys.com@gmail.com>
2022-01-05 21:01:29 +0300

    fix example api actions

--- api/actions/2.php
+++ api/actions/2.php
@@ -4,4 +4,4 @@ header("Cache-Control: no-cache");
 header("Expires: Thu, 01 Jan 1970 00:00:01 GMT");
 header("Content-Type: application/json");
 
-exit(json_encode(['status' => 'ok', 'REQUEST_URI' => $_SERVER['REQUEST_URI'], 'PHP_SELF' => $_SERVER['PHP_SELF'], 'SCRIPT_NAME' => $_SERVER['SCRIPT_NAME'], 'POST data from ajax' => $_POST]));
+exit(json_encode(['status' => 'ok', 'REQUEST_URI' => $_SERVER['REQUEST_URI'], '__FILE__' => str_replace(dirname(__FILE__, 3), '...', __FILE__), 'PHP_SELF' => $_SERVER['PHP_SELF'], 'POST data from ajax' => $_POST]));
